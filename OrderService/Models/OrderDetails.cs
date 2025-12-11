e5fd65c6b6a3af40ea1dfd0f0190f6de4ed0fbed
mohammadsheaib <mohammadsheaib@hotmail.com>
2025-12-11 09:31:43 +0200

    Adding OrderDetails class

---
+++ OrderService/Models/OrderDetails.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OrderService.Models;
+
+public class Order
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public string ProductId { get; set; }
+    public int Quantity { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
+
+public class CreateOrderDto
+{
+    public string ProductId { get; set; }
+    public int Quantity { get; set; }
+}
