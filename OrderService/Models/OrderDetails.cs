44286ab2ee4d08b80ff6fb7387aa469bdaa73125
mohammadsheaib <mohammadsheaib@hotmail.com>
2025-12-11 09:44:20 +0200

    test modification

--- OrderService/Models/OrderDetails.cs
+++ OrderService/Models/OrderDetails.cs
@@ -10,7 +10,6 @@ public class OrderDetails
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
 
-    public string ProductId { get; set; }
     public int Quantity { get; set; }
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
